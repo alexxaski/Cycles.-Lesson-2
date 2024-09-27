4e20034f295615c7561dfa15c7a558867ea9c244
VoT <alexxaski@yandex.ru>
2024-09-27 20:27:31 +0300

    HW7go

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
         int total = 0;
         int month = 0;
         while (total < 2_459_000) {
-            total = total + salary;
+            total +=  total / 100;
+            total +=  salary;
             month++;
             System.out.println("Месяц " + month + " , сумма " +
                     "накоплений равна " + total + " рублей");
@@ -39,42 +40,48 @@ public class Main {
         int monthB = 0;
         int salaryB = 15000;
         int totalB = 0;
+        int percent = 7 ;
         while (totalB <= 12_000_000) {
-            totalB = totalB + totalB * 7 / 100;
-            totalB = totalB + salaryB;
+            totalB += totalB * percent / 100 / 12;
+            totalB += salaryB;
             monthB++;
             System.out.println("Месяц " + monthB + " , сумма " +
                     "накоплений равна " + totalB + " рублей");
-
         }
-        System.out.println("Задача 5");
-
+        /* Исправил что смог , понимаю что переменные одни и те же ,
+        но как их использовать во всех заданиях объявив один раз не понимаю
+        перепробывал все , задания просто перестают выполняться .
+        Дайте подсказку.
+         */
+            System.out.println("Задача 5");
         int monthB6 = 0;
         int salaryB6 = 15000;
         int totalB6 = 0;
-
-        while (totalB6 <= 12_000_000) {
-            totalB6 = totalB6 + totalB6 * 7 / 100;
-            totalB6 = totalB6 + salaryB6;
-            monthB6++;
+        int percent6 = 7 ;
+            while (totalB6 <= 12_000_000) {
+                totalB6 += totalB6 * percent6 / 100 / 12;
+                totalB6 += salaryB6;
+                monthB6++;
             if (monthB6 % 6 == 0) {
                 System.out.println("Месяц " + monthB6 + " , сумма " +
                         "накоплений равна " + totalB6 + " рублей");
             }
         }
 
-        System.out.println("Задача 6");
+       System.out.println("Задача 6");
         int monthB9 = 0;
         int salaryB9 = 15000;
         int totalB9 = 0;
+        int percent9 = 7 ;
         while (monthB9 <= 108 ) {
-            totalB9 = totalB9 + totalB9 * 7 / 100;
-            totalB9 = totalB9 + salaryB9;
+            totalB9 += totalB9 * percent9 / 100 /12;
+            totalB9 +=  salaryB9;
             monthB9++;
                     if (monthB9 % 6 == 0) {
                     System.out.println("Cумма накоплений" +
                             " каждые пол года равна " + totalB9 + " рублей");
                 }
+
             }
 
             System.out.println("Задача 7");
@@ -85,11 +92,11 @@ public class Main {
         }
 
             System.out.println("Задача 8");
-        int yearComet = 2017 ;
+        int yearComet = 2024 ;
         int before = yearComet - 200 ;
         int after = yearComet + 100 ;
         while ( before < after ) {
-                System.out.println(before + "год прилета кометы");
+                System.out.println(before);
             before = before + 79 ;
         }
 
